f9a072d0b3eff55040a4f9c484f1a221cb750649
Tapesh <tapesh.dua420@gmail.com>
2026-03-04 00:13:14 +0530

    setup and enables loggings

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from contextlib import asynccontextmanager
 from app.db.session import engine
 from app.db.base import Base
 from app.api.routes import router as address_router
-
+from app.core.logging import setup_logging
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    setup_logging()
     Base.metadata.create_all(bind=engine)
     yield
 
